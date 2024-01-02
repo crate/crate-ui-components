0ff03a27f2821f21629302376cba0dca2f836b2a
Alex Dametto <dametto.alex@gmail.com>
2024-01-02 17:37:10 +0100

    Fixed tailwind css style rollup

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,9 @@ import typescript from "rollup-plugin-typescript2";
 import postcss from "rollup-plugin-postcss";
 import del from "rollup-plugin-delete";
 import babel from "@rollup/plugin-babel";
+import tailwindcss from "tailwindcss";
+
+const tailwindConfig = require("./tailwind.config.js");
 const packageJson = require("./package.json");
 
 const config = {
@@ -42,7 +45,10 @@ const config = {
       },
       extensions: [".css"],
       minimize: true,
-      extract: "lib.css",
+      inject: {
+        insertAt: "top",
+      },
+      plugins: [tailwindcss(tailwindConfig)],
     }),
   ],
   external: ["react", "react-dom"],
--- src/lib.ts
+++ src/lib.ts
@@ -1,2 +1,4 @@
+import "./index.css";
+
 export * from "./components";
 export { default as TestTsComponent } from "./TestTsComponent";
