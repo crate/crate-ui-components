c0ba9658d2af59d29d85dee49d7a71821eaa2ee4
Jon Combe <github@joncombe.net>
2024-01-09 14:38:11 +0700

    Export useButtonStyles

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,3 @@
-export { default as Button } from './Button';
+export { default as Button, useButtonStyles } from './Button';
 export { default as Loader } from './Loader';
 export { default as Text } from './Text';
